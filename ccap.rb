160a51c9681944016a9586c84ce88d4c535a34b7
wy <this@wysaid.org>
2026-03-08 16:50:05 +0800

    Update ccap to v1.5.1

--- ccap.rb
+++ ccap.rb
@@ -1,8 +1,8 @@
 class Ccap < Formula
   desc "High-performance cross-platform camera capture library with hardware-accelerated pixel format conversion and complete C++/C APIs"
   homepage "https://github.com/wysaid/CameraCapture"
-  url "https://github.com/wysaid/CameraCapture/archive/refs/tags/v1.5.0.tar.gz"
-  sha256 "e617fd973c27f6f9a9f5ec831e4440430d5ae5fd2ab3a959b3a2ca7be031162e"
+  url "https://github.com/wysaid/CameraCapture/archive/refs/tags/v1.5.1.tar.gz"
+  sha256 "36cf27af6c79c6496be1b763355efb88b84172d14a9910e180a228d1c363d919"
   license "MIT"
   head "https://github.com/wysaid/CameraCapture.git", branch: "main"
 
